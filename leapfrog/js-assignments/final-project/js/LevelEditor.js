a1e3e0a27745927d96bbed44d29deb5c82339756
shantanubhattarai <shantanu.bhattarai271@gmail.com>
2020-07-16 13:51:25 +0545

    Initialize Level Editor

---
+++ leapfrog/js-assignments/final-project/js/LevelEditor.js
@@ -0,0 +1,84 @@
+class LevelEditor{
+  constructor(){
+    this.layers = [[],[]];
+    this.placedTiles = new Array(mainMap.cols * mainMap.rows);
+    this.mapHeight = mainMap.rows * mainMap.tsize;
+    this.mapWidth = mainMap.cols * mainMap.tsize;
+    this.canvas = document.querySelector('#main-canvas');
+    this.context = this.canvas.getContext('2d');
+    this.canvas.style.display = 'inline-block';
+    this.canvas.width = mainMap.cols * mainMap.tsize;
+    this.canvas.height = 1300;
+    this.sourceWidth = 375;
+    this.sourceHeight = 530;
+    this.canvas.addEventListener('click', this.clickEventHandler);
+    this.canvas.addEventListener('mousemove', this.mouseMoveEventHandler);
+    this.roadTiles = new Image();
+    this.roadTiles.src = './img/RoadTiles.png';
+
+    this.roadTiles.addEventListener('load', this.redrawImage);
+
+    this.drawGrid();
+    this.initLowerLayer();
+  }
+
+  drawGrid = () => {
+    for(let i = 0; i <= mainMap.cols; i++){
+      this.context.moveTo(i * mainMap.tsize, 0);
+      this.context.lineTo(i * mainMap.tsize, this.mapHeight);
+    }
+    this.context.stroke();
+
+    for(let i = 0; i <= mainMap.rows; i++){
+      this.context.moveTo(0, i * mainMap.tsize);
+      this.context.lineTo(this.mapWidth, i * mainMap.tsize);
+    }
+    this.context.stroke();
+  }
+
+  redrawImage = () => {
+    this.context.drawImage(this.roadTiles, 0, 0, this.sourceWidth, this.sourceHeight, 0, this.mapHeight, this.sourceWidth, this.sourceHeight);
+  }
+
+  clickEventHandler = () => {
+
+  }
+
+  mouseMoveEventHandler = () => {
+
+  }
+
+
+  initLowerLayer = () => {
+    this.layers[0] = [
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,6,27,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,6,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    ];
+  }
+}
--- leapfrog/js-assignments/final-project/js/Starter.js
+++ leapfrog/js-assignments/final-project/js/Starter.js
@@ -2,17 +2,36 @@ class Starter {
   constructor(){
     this.gameButton = document.querySelector('#game-button');
     this.gameButton.addEventListener('click', startGame);
+
+    this.loadLevelButton = document.querySelector('#load-level-button');
+    this.loadLevelButton.addEventListener('click', startLevelLoader);
+
+    this.levelEditorButton = document.querySelector('#level-editor-button');
+    this.levelEditorButton.addEventListener('click', startLevelEditor);
   }
 }
 
 var mainGameLoop;
-
 
 var startGame = () => {
   var startMenuContainer = document.querySelector('.start-menu');
   startMenuContainer.style.display = 'none';
   initializePlayers();
   mainGameLoop = new MainGameLoop();
+}
+
+var levelEditor;
+
+var startLevelEditor = () => {
+  var startMenuContainer = document.querySelector('.start-menu');
+  startMenuContainer.style.display = 'none';
+  levelEditor = new LevelEditor();
+}
+
+var levelLoader;
+
+var startLevelLoader = () => {
+
 }
 
 var starter = new Starter();
