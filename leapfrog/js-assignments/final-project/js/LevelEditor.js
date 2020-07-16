d240ec7575d1adc4830f406bc45e679b8341cb82
shantanubhattarai <shantanu.bhattarai271@gmail.com>
2020-07-16 20:16:30 +0545

    Add base grid and base tiles to level editor

--- leapfrog/js-assignments/final-project/js/LevelEditor.js
+++ leapfrog/js-assignments/final-project/js/LevelEditor.js
@@ -8,18 +8,17 @@ class LevelEditor{
     this.context = this.canvas.getContext('2d');
     this.canvas.style.display = 'inline-block';
     this.canvas.width = mainMap.cols * mainMap.tsize;
-    this.canvas.height = 1300;
+    this.canvas.height = 800;
     this.sourceWidth = 375;
     this.sourceHeight = 530;
     this.canvas.addEventListener('click', this.clickEventHandler);
     this.canvas.addEventListener('mousemove', this.mouseMoveEventHandler);
-    this.roadTiles = new Image();
-    this.roadTiles.src = './img/RoadTiles.png';
-
-    this.roadTiles.addEventListener('load', this.redrawImage);
-
+    this.mapTilesImage = document.createElement('img');
+    this.mapTilesImage.src = './img/RoadTiles.png';
+    this.tilesToDraw = [1, 27, 77, 79, 81, 10, 76, 56, 12, 2, 3, 5, 7, 28, 29, 111, 112, 113, 155, 156, 157, 137, 120, 122, 141, 134, 135, 500, 501];
     this.drawGrid();
     this.initLowerLayer();
+    this.render();
   }
 
   drawGrid = () => {
@@ -33,11 +32,8 @@ class LevelEditor{
       this.context.moveTo(0, i * mainMap.tsize);
       this.context.lineTo(this.mapWidth, i * mainMap.tsize);
     }
+    this.context.strokeStyle = '#aaa';
     this.context.stroke();
-  }
-
-  redrawImage = () => {
-    this.context.drawImage(this.roadTiles, 0, 0, this.sourceWidth, this.sourceHeight, 0, this.mapHeight, this.sourceWidth, this.sourceHeight);
   }
 
   clickEventHandler = () => {
@@ -48,7 +44,6 @@ class LevelEditor{
 
   }
 
-
   initLowerLayer = () => {
     this.layers[0] = [
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -57,7 +52,6 @@ class LevelEditor{
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-      1,6,27,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -71,7 +65,8 @@ class LevelEditor{
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-      1,6,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+      1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -81,4 +76,64 @@ class LevelEditor{
       1,1,1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
     ];
   }
+
+  drawInteratableTiles(){
+    for(let c = 0; c < mainMap.cols; c++){
+      for (let r = 0; r < mainMap.rows; r++){
+        var tile = this.getInteractableTiles(c,r);
+        if (tile !== 0){
+          this.context.drawImage(
+            this.mapTilesImage,
+            (tile > 22) ? ((tile % 22 - 1) * mainMap.sourceSize)+1 : (tile - 1) * mainMap.sourceSize + 1,
+            (tile > 22) ? (Math.floor(tile/22) * mainMap.sourceSize)+1 : 1,
+            mainMap.sourceSize-1,
+            mainMap.sourceSize-1,
+            c * (mainMap.tsize),
+            this.mapHeight + 10 + r * (mainMap.tsize),
+            mainMap.tsize,
+            mainMap.tsize
+          );
+
+        }
+      }
+    }
+  }
+
+  getTileFromNewMap(layer, col, row){
+    return this.layers[layer][row * mainMap.cols + col];
+  }
+
+  getInteractableTiles(col,row){
+    return this.tilesToDraw[row * mainMap.cols + col];
+  }
+
+  drawLayer = (layer) => {
+    for(let c = 0; c < mainMap.cols; c++){
+      for (let r = 0; r < mainMap.rows; r++){
+        var tile = this.getTileFromNewMap(layer, c, r);
+        if (tile !== 0){
+          this.context.drawImage(
+            this.mapTilesImage,
+            (tile > 22) ? ((tile % 22 - 1) * mainMap.sourceSize)+1 : (tile - 1) * mainMap.sourceSize + 1,
+            (tile > 22) ? (Math.floor(tile/22) * mainMap.sourceSize)+1 : 1,
+            mainMap.sourceSize-1,
+            mainMap.sourceSize-1,
+            c * (mainMap.tsize),
+            r * (mainMap.tsize),
+            mainMap.tsize,
+            mainMap.tsize
+          );
+        }
+      }
+    }
+  }
+
+  render = () => {
+    this.drawLayer(0);
+    this.drawGrid();
+    this.drawLayer(1);
+    this.drawInteratableTiles();
+    window.requestAnimationFrame(this.render);
+  }
+
 }
