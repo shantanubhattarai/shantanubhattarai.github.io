12e8cfc3d070b3a64948a03b499c6399ad12d7b7
shantanubhattarai <shantanu.bhattarai271@gmail.com>
2020-07-16 21:57:22 +0545

    Add ability to click and put tiles in level editor

--- leapfrog/js-assignments/final-project/js/LevelEditor.js
+++ leapfrog/js-assignments/final-project/js/LevelEditor.js
@@ -8,7 +8,7 @@ class LevelEditor{
     this.context = this.canvas.getContext('2d');
     this.canvas.style.display = 'inline-block';
     this.canvas.width = mainMap.cols * mainMap.tsize;
-    this.canvas.height = 800;
+    this.canvas.height = 850;
     this.sourceWidth = 375;
     this.sourceHeight = 530;
     this.canvas.addEventListener('click', this.clickEventHandler);
@@ -16,28 +16,42 @@ class LevelEditor{
     this.mapTilesImage = document.createElement('img');
     this.mapTilesImage.src = './img/RoadTiles.png';
     this.tilesToDraw = [1, 27, 77, 79, 81, 10, 76, 56, 12, 2, 3, 5, 7, 28, 29, 111, 112, 113, 155, 156, 157, 137, 120, 122, 141, 134, 135, 500, 501];
+    this.sourceTile = 0;
     this.drawGrid();
     this.initLowerLayer();
     this.render();
   }
 
-  drawGrid = () => {
-    for(let i = 0; i <= mainMap.cols; i++){
-      this.context.moveTo(i * mainMap.tsize, 0);
-      this.context.lineTo(i * mainMap.tsize, this.mapHeight);
-    }
-    this.context.stroke();
-
-    for(let i = 0; i <= mainMap.rows; i++){
-      this.context.moveTo(0, i * mainMap.tsize);
-      this.context.lineTo(this.mapWidth, i * mainMap.tsize);
-    }
-    this.context.strokeStyle = '#aaa';
-    this.context.stroke();
+  getTileFromNewMap(layer, col, row){
+    return this.layers[layer][row * mainMap.cols + col];
   }
 
-  clickEventHandler = () => {
+  getInteractableTiles(col,row){
+    return this.tilesToDraw[row * mainMap.cols + col];
+  }
 
+  clickEventHandler = (e) => {
+    let rect = this.canvas.getBoundingClientRect();
+    let mousePos = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    }
+
+    if(mousePos.y > this.mapHeight + 10 && mousePos.y < (this.mapHeight + 10 + mainMap.tsize) && mousePos.x < this.tilesToDraw.length * mainMap.tsize){
+      let tileX = Math.floor(mousePos.x/ mainMap.tsize);
+      let tileY = Math.floor((mousePos.y - this.mapHeight - 10)/ mainMap.tsize);
+      let sourceIndex = tileY * this.tilesToDraw.length + tileX;
+      this.sourceTile = this.tilesToDraw[sourceIndex];
+    }
+
+    if(mousePos.y < this.mapHeight && mousePos.x < mainMap.cols * mainMap.tsize){
+      let tileX = Math.floor(mousePos.x/ mainMap.tsize);
+      let tileY = Math.floor(mousePos.y/ mainMap.tsize);
+      let targetTile = tileY * mainMap.cols + tileX;
+      this.layers[1][targetTile] = this.sourceTile;
+    }
+
+    //replace layers[1][x] on click, x is tilePos where clicked
   }
 
   mouseMoveEventHandler = () => {
@@ -77,9 +91,9 @@ class LevelEditor{
     ];
   }
 
-  drawInteratableTiles(){
-    for(let c = 0; c < mainMap.cols; c++){
-      for (let r = 0; r < mainMap.rows; r++){
+  drawInteratableTiles = () => {
+    for(let c = 0; c < this.tilesToDraw.length; c++){
+      for (let r = 0; r < 2; r++){
         var tile = this.getInteractableTiles(c,r);
         if (tile !== 0){
           this.context.drawImage(
@@ -93,18 +107,34 @@ class LevelEditor{
             mainMap.tsize,
             mainMap.tsize
           );
-
         }
       }
     }
-  }
+    for(let c = 0; c < 4; c++){
+      /*this.context.drawImage(
+        mainSpriteSheet,
+        this.spritePos[this.color].x,
+        this.spritePos[this.color].y,
+        mainMap.sourceSize-1,
+        mainMap.sourceSize-1,
+        this.x,
+        this.y,
+        mainMap.tsize,
+        mainMap.tsize
+      );*/
+      this.context.drawImage(
+        mainSpriteSheet,
+        3,
+        104,
+        mainMap.sourceSize-1,
+        mainMap.sourceSize-1,
+        c * (mainMap.tsize),
+        this.mapHeight + 50,
+        mainMap.tsize,
+        mainMap.tsize
+      );
+    }
 
-  getTileFromNewMap(layer, col, row){
-    return this.layers[layer][row * mainMap.cols + col];
-  }
-
-  getInteractableTiles(col,row){
-    return this.tilesToDraw[row * mainMap.cols + col];
   }
 
   drawLayer = (layer) => {
@@ -126,6 +156,21 @@ class LevelEditor{
         }
       }
     }
+  }
+
+  drawGrid = () => {
+    for(let i = 0; i <= mainMap.cols; i++){
+      this.context.moveTo(i * mainMap.tsize, 0);
+      this.context.lineTo(i * mainMap.tsize, this.mapHeight);
+    }
+    this.context.stroke();
+
+    for(let i = 0; i <= mainMap.rows; i++){
+      this.context.moveTo(0, i * mainMap.tsize);
+      this.context.lineTo(this.mapWidth, i * mainMap.tsize);
+    }
+    this.context.strokeStyle = '#aaa';
+    this.context.stroke();
   }
 
   render = () => {
